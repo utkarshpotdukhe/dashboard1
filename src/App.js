656655741508ebb4afcfdde1657f31a5e6c98fc2
Utkarsh Potdukhe <utkarsh16potdukhe@gmail.com>
2024-03-20 19:01:10 +0530

    dasboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import LoaderComponent from './components/LoaderComponent/LoaderComponent';
 
 function App() {
+  // Assuming other components and logic exist here
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Other components */}
+      <LoaderComponent />
     </div>
   );
 }
